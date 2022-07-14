6594b3c7b0fd974cd44699e9563d5fe4f48ce884
Senfire <eric.mcghee@northwestern.edu>
2022-07-14 16:16:49 -0500

    Array is created but not sure how to implement it yet, time for some research

--- Assets/script.js
+++ Assets/script.js
@@ -11,6 +11,62 @@ var myPlans = [
         timeOfDay: "am",
         notes: "",
     },
+    {
+        id: "2",
+        hour: "10",
+        time: "10",
+        timeOfDay: "am",
+        notes: "",
+    },
+    {
+        id: "3",
+        hour: "11",
+        time: "11",
+        timeOfDay: "am",
+        notes: "",
+    },
+    {
+        id: "4",
+        hour: "12",
+        time: "12",
+        timeOfDay: "pm",
+        notes: "",
+    },
+    {
+        id: "5",
+        hour: "01",
+        time: "13",
+        timeOfDay: "pm",
+        notes: "",
+    },
+    {
+        id: "6",
+        hour: "02",
+        time: "14",
+        timeOfDay: "pm",
+        notes: "",
+    },
+    {
+        id: "7",
+        hour: "03",
+        time: "15",
+        timeOfDay: "pm",
+        notes: "",
+    },
+    {
+        id: "8",
+        hour: "04",
+        time: "16",
+        timeOfDay: "pm",
+        notes: "",
+    },
+    {
+        id: "9",
+        hour: "05",
+        time: "17",
+        timeOfDay: "pm",
+        notes: "",
+    },
 ]
 
 
