d289ef1870960794282f292bd5af6381e6123cd6
Senfire <eric.mcghee@northwestern.edu>
2022-07-14 15:37:45 -0500

    Adding the date to the jumbo tron

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,12 @@
+//Varibles and test varibles
+var currentDate = moment().format('dddd,  MMM Do');
+
+
 //display the current day in the JumboTron of the Website.
 
-function headerDate() {
-    var currentDate = moment().format('dddd,  MMM Do');
-    $('#currentDay').text(currentDate);
+function dateUpdate() {
+    document.getElementById('currentDay').innerHTML = currentDate;
 }
+dateUpdate();
 
-
+//
