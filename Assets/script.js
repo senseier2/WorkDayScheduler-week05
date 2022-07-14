031bfcf9297dd722115c00f98e467a886c8bd469
Senfire <eric.mcghee@northwestern.edu>
2022-07-14 16:12:28 -0500

    Creating an Array of data points for data collection

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,18 @@
 //Varibles and test varibles
 var currentDate = moment().format('dddd,  MMM Do');
 
+//Creating an Array of data objects for the planner data
+
+var myPlans = [
+    {
+        id: "1",
+        hour: "09",
+        time: "09",
+        timeOfDay: "am",
+        notes: "",
+    },
+]
+
 
 //display the current day in the JumboTron of the Website.
 
