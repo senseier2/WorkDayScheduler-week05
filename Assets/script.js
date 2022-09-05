f978487e3ddebf9b70380f849d5f2cc4a4b9dc00
Senfire <eric.mcghee@northwestern.edu>
2022-09-05 17:21:24 -0500

    adding the time tracker function and logic areas for updating the styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,38 @@
 //Varibles and test varibles
 
+const { on } = require("nodemon");
+
 //display the current day in the JumboTron of the Website.
 var dateToday = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").html(dateToday);
 
+//function to set item to local storage on click
+$(document).ready(function() {
+  $(".saveBtn")on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+
+  //Tracking time and updating the color status of the time blocks
+  function trackTime() {
+    var timeCurrent = moment().hour();
+
+    $(".time-block").each(function () {
+      var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+      //If then to handle the styling changes "past present and future"
+      if (timeBlock < timeCurrent) {
+
+      } else if (timeBlock === timeCurrent) {
+
+
+      } else {
+
+      }
+
+    })
+  }
+
+
+});
